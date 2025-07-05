25bb1c048af8890ca513aa254dc8e63c4b787288
Shorno <fb.shorno@gmail.com>
2025-07-05 23:16:02 +0600

    fix : (api not running at build time)

--- data/getAgents.ts
+++ data/getAgents.ts
@@ -8,11 +8,8 @@ interface AgentApiResponse {
     status: string
 }
 
-//for mocking real api
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export async function getAgents(): Promise<AgentApiResponse> {
     await new Promise(resolve => setTimeout(resolve, 500));
-    // const response = await fetch(`${API_URL}/agents`)
     return {
         message : "Agent mock-data retrieved successfully",
         status: "success",
